fcf7f85cabdcd253a72f85460b3de9b0d3ca6532
kyoseicyber <cyberkyosei0821@gmail.com>
2025-12-04 00:30:41 +0900

    add dog api

--- routes/dog.js
+++ routes/dog.js
@@ -2,13 +2,16 @@ var express = require('express');
 var router = express.Router();
 const request = require('request');
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
     request('https://dog.ceo/api/breeds/image/random', function (error, response, body) {
         if (!error && response.statusCode == 200) {
             const data = JSON.parse(body);
-            res.json(data);
+            res.render('dog', { dogImage: data.message }); 
+        } else {
+            res.send("犬画像を取得できませんでした");
         }
     });
 });
 
 module.exports = router;
+
